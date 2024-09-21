443c418adf6d5dbb687b34c66995b5df754de184
rogermoretti <105942962+rogermoretti@users.noreply.github.com>
2024-09-20 22:15:39 -0300

    Add files via upload

---
+++ geradordesenhas-main/main.js
@@ -0,0 +1,5 @@
+contnumeroSenha = document.querySelector('.parametro-senha_texto')
+lettamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+constletrasmaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+constletrasmaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
