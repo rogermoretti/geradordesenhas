6b6689355d0cc837acfd161261f9fcd7dbab59d8
rogermoretti <105942962+rogermoretti@users.noreply.github.com>
2024-09-27 22:11:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,4 +2,20 @@ contnumeroSenha = document.querySelector('.parametro-senha_texto')
 lettamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 constletrasmaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-constletrasmaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+constletrasmaiuscula = 'abcdefghijklmnopqrstuvwxyz'
+constnumero = '0123456789';
+constsimbolos = '!@%*?';
+constbotoes = document.querySelectorAll('.parametro-senha_botao');
+constcampoSenha = document.querySelector('#campo_senha');
+constcheckbox = document.querySelectorAll('.checkbox');
+constForcaSenha = document.querySelector('.forca');
+botoes[0].onclik=diminuiTamanho;
+botoes[1].onclik=aumentaTamanho;
+
+functiondiminuiTamanho(){
+    if (tamanhoSenha < 1){
+        //tamanhoSenha = tamanhoSenha-1){
+
+        
+    
+  
